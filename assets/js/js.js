53ec824c600836992b6e19125284a4b2a0053307
Osvaldo Fernandez <osvaldofernandez@KAIS-MAC43.local>
2018-06-29 18:34:15 +0200

    Auto-generated commit

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,11 @@
-$("#menu-toggle").click(function(e) {
+$("#menu-toggle").click(function (e) {
+  console.log('hallo');
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+  $("#service").toggleClass("toggled");
+});
+
+$("#service").click(function (e) {
   console.log('hallo');
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
